b39acd0215cf81f146906c1db577c98d541716c9
Francis Githae <francizgithae@gmail.com>
2022-05-26 15:23:31 +0300

    add google map to react application

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import IntroSection from './components/intro/Intro'
 import ContactSection from './components/contact-section/ContactSection'
 import DisclaimerSection from './components/disclaimer/Disclaimer'
 import FooterSection from './components/footer/Footer'
-
+import Map from './components/map/Map'
 import './App.css'
+
+const location={
+  address:'Kenyatta International Convention Centre',
+  lat:-1.288693,
+  lng:36.823103
+}
 
 function App() {
   return (
     <div className="App">
       <IntroSection />
       <ContactSection />
+      <Map location={location} zoomLevel={17} />
       <DisclaimerSection />
       <FooterSection />
     </div>
---
+++ src/components/map/Map.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import {Icon} from '@iconify/react'
+
+import locationIcon from '@iconify/icons-mdi/map-marker'
+import './map.css'
+
+const googleMapsAPIKey=process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+console.log(googleMapsAPIKey)
+
+const LocationPin=({text})=>(
+    <div className='pin'>
+        <Icon icon={locationIcon} className='pin-icon'/>
+        <p className='pin-text'>{text}</p>
+    </div>
+)
+
+const Map=({location,zoomLevel})=>(
+    <div className='map'>
+
+        <h2 className='map-h2'>Visit our iconic building</h2>
+
+        <div className='google-map'>
+            <GoogleMapReact 
+            bootstrapURLKeys={{key:googleMapsAPIKey}}
+            defaultCenter={location}
+            defaultZoom={zoomLevel}
+            >
+            <LocationPin  
+            lat={location.lat}
+            lng={location.lng}
+            text={location.address}
+            />
+
+            </GoogleMapReact>
+
+        </div>
+    
+    </div>
+)
+
+export default Map
